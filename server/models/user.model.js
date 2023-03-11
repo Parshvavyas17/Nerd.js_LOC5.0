992957ec40ee8508d4abc5a43e0fd4473b6e6caf
Parshvavyas17 <parshvavyas07@gmail.com>
2023-03-11 16:00:50 +0530

    Chat partial

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -47,6 +47,14 @@ const userSchema = new mongoose.Schema(
     },
     avatar: {
       type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
     },
   },
   {
--- server/routers/user.route.js
+++ server/routers/user.route.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const { login, signup } = require("../controllers/user.controller");
+const {
+  login,
+  signup,
+  getSelf,
+  getAllUsers,
+} = require("../controllers/user.controller");
+const authUser = require("../middleware/userAuth");
 
 const router = express.Router();
 
 router.post("/login", login);
 
 router.post("/signup", signup);
+
+router.get("/self", authUser, getSelf);
+
+router.get("/all", getAllUsers);
 
 module.exports = router;
