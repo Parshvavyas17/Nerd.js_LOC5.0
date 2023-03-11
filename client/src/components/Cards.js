29a8461800fed2dc09968bc11302957270f32808
prem <premdoshi1234@gmail.com>
2023-03-11 20:05:16 +0530

    Folders

---
+++ client/src/components/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Cards = () => {
+    return (
+        <div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 ml-10">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Research</div>
+                    <p class="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
+
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
+import Cards from '../components/Cards'
 
 function Landing() {
   return (
@@ -10,8 +11,20 @@ function Landing() {
         </div>
         <div className="mx-32 my-10">
         <Banner/>
-        </div>
-    </div>
+      </div>
+      <div className="mt-48 bg-gradient-to-r from-violet-500 to-fuchsia-500 ">
+            <h1 className="font-bold flex justify-evenly items-center text-black text-5xl"> About Us</h1>
+            <h1 className="font-bold flex justify-evenly items-center text-black text-2xl">Our Goals</h1>
+            <div className="flex mt-10 justify-evenly">
+            <Cards/>
+            <Cards/>
+            <Cards/>
+           
+            </div>
+
+
+      </div>
+      </div>
   )
 }
 
