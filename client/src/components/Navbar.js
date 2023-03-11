884ba6b3290b50f7f0ee4ccf137f72eea7a68d1f
RichaRokadiya <richarajeshrokadiya@gmail.com>
2023-03-11 13:07:31 +0530

    Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import airbnb from '../Assets/airbnb.svg'
+// import airbnb from '../Assets/airbnb.svg'
  import { AiOutlineSearch } from "react-icons/ai";
 
 const Navbar = () => {
@@ -8,7 +8,7 @@ const Navbar = () => {
             {/* left */}
             <div className="header h-24 bg-white  grid grid-cols-3 shadow-md ">
                 <div className="image">
-                    <img src={airbnb} alt="logo" className="h-16 ml-10 px-10 mt-4 cursor-pointer"></img>
+                    <img src='#' alt="logo" className="h-16 ml-10 px-10 mt-4 cursor-pointer"></img>
                 </div>
                 {/* Middle */}
                 <div className="search bg-white w-3/5 h-12 rounded-full py-2 mt-6 shadow-lg border px-2 ">
